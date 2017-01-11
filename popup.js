9525d5324dfd86e156701881e44542a354d1a8b8
Michael Hailu Kebede <Kochiiiiii@gmail.com>
2017-01-10 22:12:34 -0500

    Add function to prevent duplicates and refactor some functions

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// Main function that gets called when the import button is clicked
 function importBookmarks() {
     // Bring data from the front end
     var data = $('#exportedDeliciousBookmarks').val();
@@ -8,36 +9,45 @@ function importBookmarks() {
     for (var key in bookmarksObj) {
         var tag = bookmarksObj[key]["tags"][0].toLowerCase(); //just use the first tag since chrome bookmarks cant do multiple tags
         var bookmarksArr = map.get(tag);
-        if (bookmarksArr == undefined) {
-            bookmarksArr = new Array(bookmarksObj[key]);
-            map.set(tag, bookmarksArr);
+        if (map.has(tag)) {
+            // Do not add bookmark if already in array. Prevent duplicate bookmarks.
+            if(!isObjInArray(bookmarksObj[key], bookmarksArr)){
+              bookmarksArr.push(bookmarksObj[key]);
+              map.set(tag, bookmarksArr);
+            }
         } else {
-            bookmarksArr.push(bookmarksObj[key]);
+            bookmarksArr = new Array(bookmarksObj[key]);
             map.set(tag, bookmarksArr);
         }
     }
-
-    // User chrome bookmarks API to create the root folder 'Imported from Delicious' and create a folder
-    // for each tag under the root folder and add the bookmarks to the tag folders respectively
-    chrome.bookmarks.getTree(
-        function(node) {
-            bookmarkBarNode = node[0].children[0]; //node location for chrome 'bookmark bar'
-            chrome.bookmarks.create({
-                    'parentId': bookmarkBarNode.id,
-                    'title': 'Imported From Delicious'
-                },
-                function(importFolder) {
-                    // Iterate through the map, create a chrome folder for each tag and add the bookmarks using the chrome api
-                    for (var mapToArr of map) {
-                        var tag = mapToArr[0];
-                        var bookmarksArr = mapToArr[1];
-                        addBookmarks(importFolder.id, tag, bookmarksArr);
-                    }
-                });
-        });
+    // Pass the reformatted data to the function and add them to the chrome bookmarks bar
+    addBookmarks(map);
 }
 
-function addBookmarks(importFolderId, tag, bookmarksArr) {
+// Function that uses chrome bookmarks API to create the root folder 'Imported from Delicious' and create a folder
+// for each tag under the root folder and adds the bookmarks to the tag folders respectively
+function addBookmarks(map){
+  chrome.bookmarks.getTree(
+      function(node) {
+          bookmarkBarNode = node[0].children[0]; //node location for chrome 'bookmark bar'
+          chrome.bookmarks.create({
+                  'parentId': bookmarkBarNode.id,
+                  'title': 'Imported From Delicious'
+              },
+              function(importFolder) {
+                  // Iterate through the map, create a chrome folder for each tag and add the bookmarks using the chrome bookmark api
+                  for (var mapToArr of map) {
+                      var tag = mapToArr[0];
+                      var bookmarksArr = mapToArr[1];
+                      addTagsAndBookmarks(importFolder.id, tag, bookmarksArr);
+                  }
+              });
+      });
+}
+
+// Function that uses chrome bookmarks api to add the folder
+// for tag and add all bookmarks for that tag to folder
+function addTagsAndBookmarks(importFolderId, tag, bookmarksArr) {
     chrome.bookmarks.create({
             'parentId': importFolderId,
             'title': tag
@@ -53,6 +63,18 @@ function addBookmarks(importFolderId, tag, bookmarksArr) {
         });
 }
 
+// Function that checks if bookmark is already in the list of bookmarks array
+function isObjInArray(bookmarksObj, bookmarksArr){
+  for(let obj of bookmarksArr){
+    if(obj.title == bookmarksObj.title && obj.url == bookmarksObj.url){
+      return true;
+    }
+  }
+  return false;
+}
+
+// Function used only for debugging purposes
+// Print to console the first couple of Bookmark nodes
 function traverseNodesPlain(){
   var node = chrome.bookmarks.getTree(
     function(node){
@@ -69,6 +91,8 @@ function traverseNodesPlain(){
   );
 }
 
+// Function used only for debugging purposes
+// Print to console the all the contents of the chrome bookmark nodes
 function traverseNodesRecursively(){
   chrome.bookmarks.getTree(
     //call back function per the specification of the .getTree method
@@ -99,6 +123,7 @@ function traverseNodesRecursively(){
   );
 }
 
+// Add EventListener for the import button
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("import_button").addEventListener("click", importBookmarks);
     //createDeliciousFolder();
