508c3a9c99b4e977dff68c1667126c2c0b0ceb43
Michael Hailu Kebede <Kochiiiiii@gmail.com>
2017-01-14 22:06:47 -0500

    Final commit before pushing to github

---
+++ README.md
@@ -0,0 +1,46 @@
+# import-bookmarks-to-chrome
+Chrome extension for bulk import of bookmarks into Chrome.
+
+### Installing
+1. Download or clone this repository `git clone https://github.com/mikematic/import-bookmarks-to-chrome.git`
+2. Open (chrome://extensions/) or select the menu `Window > Extensions`.
+3. Enable the developer mode at top right.
+4. Click `Load unpacked extension...` and select the source code folder.
+5. You're ready to import!
+
+### Using
+1. Click the blue Kinopio icon for the extension
+
+2. Paste your import data in the JSON format below in the text area
+
+### Sample input JSON for import
+```json
+      [{
+          "title": "Google",
+          "url": "https://www.google.com/",
+          "tags": [
+            "search engine",
+            "cloud"
+          ]
+        },
+        {
+         "title": "Amazon",
+         "url": "https://www.amazon.com",
+         "tags": [
+           "shopping"
+         ]
+       }]
+```
+
+3. Click import!
+
+4. The bookmarks will be added in a folder named 'Imported From Delicious' under the bookmarks bar
+
+### More info
+The main purpose I developed this extension was to help in bulk importing of my del.icio.us bookmarks into chrome. [del.icio.us](https://del.icio.us/) seems to be currently running out of business and I needed to migrate my bookmarks into another reliable bookmark manager like Chrome. However, del.icio.us have disabled the feature to export bookmarks and I had more than one thousand bookmarks that were kind of held hostage. I was able to download my bookmarks using the wget tool albeit in html format cluttered with a lot of html tags. I wrote a Java [program](https://github.com/mikematic/deliciousparser) to parse these htmls into one clean JSON input that can be used by this extension to import into Chrome bookmarks. For more details about the export and import process look into the instructions of the Java program [deliciousparser](https://github.com/mikematic/deliciousparser) that is also available on github.
+
+### Contributions
+Stick a fork at it
+
+### License
+Apache
--- popup.js
+++ popup.js
@@ -1,129 +1,76 @@
+// Global variable to store total number of bookmarks to be imported
+var numBkmrks;
+
 // Main function that gets called when the import button is clicked
-function importBookmarks() {
+function importBkmrks() {
+    // Change UI to reflect processing state
+    document.getElementById("loading_img").style.display = "block";
+    document.getElementById("importTxtArea").style.display = "none";
+    document.getElementById("import_button").style.display = "none";
+    document.getElementById("instructionDiv").style.display = "none";
+
     // Bring data from the front end
-    var data = $('#exportedDeliciousBookmarks').val();
-    var bookmarksObj = JSON.parse(data);
+    var data = $('#importTxtArea').val();
+    var bkmrksArr = JSON.parse(data);
+    numBkmrks = bkmrksArr.length;
 
-    // Re-format the data and put it into a Map object {<tag>:[<bookmarkObj>]...}
-    var map = new Map();
-    for (var key in bookmarksObj) {
-        var tag = bookmarksObj[key]["tags"][0].toLowerCase(); //just use the first tag since chrome bookmarks cant do multiple tags
-        var bookmarksArr = map.get(tag);
-        if (map.has(tag)) {
-            // Do not add bookmark if already in array. Prevent duplicate bookmarks.
-            if(!isObjInArray(bookmarksObj[key], bookmarksArr)){
-              bookmarksArr.push(bookmarksObj[key]);
-              map.set(tag, bookmarksArr);
-            }
-        } else {
-            bookmarksArr = new Array(bookmarksObj[key]);
-            map.set(tag, bookmarksArr);
-        }
-    }
-    // Pass the reformatted data to the function and add them to the chrome bookmarks bar
-    addBookmarks(map);
-}
-
-// Function that uses chrome bookmarks API to create the root folder 'Imported from Delicious' and create a folder
-// for each tag under the root folder and adds the bookmarks to the tag folders respectively
-function addBookmarks(map){
-  chrome.bookmarks.getTree(
-      function(node) {
-          bookmarkBarNode = node[0].children[0]; //node location for chrome 'bookmark bar'
-          chrome.bookmarks.create({
-                  'parentId': bookmarkBarNode.id,
-                  'title': 'Imported From Delicious'
-              },
-              function(importFolder) {
-                  // Iterate through the map, create a chrome folder for each tag and add the bookmarks using the chrome bookmark api
-                  for (var mapToArr of map) {
-                      var tag = mapToArr[0];
-                      var bookmarksArr = mapToArr[1];
-                      addTagsAndBookmarks(importFolder.id, tag, bookmarksArr);
-                  }
-              });
-      });
-}
-
-// Function that uses chrome bookmarks api to add the folder
-// for tag and add all bookmarks for that tag to folder
-function addTagsAndBookmarks(importFolderId, tag, bookmarksArr) {
+    // Uses chrome bookmarks API to create the root folder 'Imported from Delicious' and create a folder
+    // for each tag under the root folder and adds the bookmarks to the tag folders respectively
     chrome.bookmarks.create({
-            'parentId': importFolderId,
-            'title': tag
+            'parentId': '1', // Id for Bookmark Bar Node
+            'title': 'Imported from Delicious'
         },
-        function(newTagFolder) {
-            bookmarksArr.forEach(function(bookmark) {
-                chrome.bookmarks.create({
-                    parentId: newTagFolder.id,
-                    title: bookmark.title,
-                    url: bookmark.url
-                });
-            });
+        function(importFolder) {
+            outerFunc(importFolder.id, bkmrksArr);
         });
 }
 
-// Function that checks if bookmark is already in the list of bookmarks array
-function isObjInArray(bookmarksObj, bookmarksArr){
-  for(let obj of bookmarksArr){
-    if(obj.title == bookmarksObj.title && obj.url == bookmarksObj.url){
-      return true;
+function outerFunc(importFolderId, bkmrksArr) {
+    if (bkmrksArr.length > 0) {
+        var currObj = bkmrksArr.pop();
+        chrome.bookmarks.search(currObj.tags[0], function callback(searchResultNodesArr) {
+            innerFunc(importFolderId, searchResultNodesArr, currObj, bkmrksArr);
+        });
     }
-  }
-  return false;
+    else{
+      // Change UI to reflect completed state
+      document.getElementById("loading_img").style.display = "none";
+      document.getElementById("completedMessage").style.display = "block";
+      document.getElementById("completedMessage").innerHTML = '<h1>' + numBkmrks + ' bookmarks imported!</h1>';
+    }
 }
 
-// Function used only for debugging purposes
-// Print to console the first couple of Bookmark nodes
-function traverseNodesPlain(){
-  var node = chrome.bookmarks.getTree(
-    function(node){
-        var parent = node[0].children[0];
-        var firstChild = parent.children[0]
-        var firstGrandChild = firstChild.children[0];
-        console.log('parent.title: ' + parent.title);
-        console.log('firstChild.title: ' + firstChild.title);
-        console.log('firstGrandChild.title: ' + firstGrandChild.title);
-        for(i=0; i < node[0].children.length; i++){
-          console.log('node[0].children[i]: ' + node[0].children[i].title);
+function innerFunc(importFolderId, searchResultNodesArr, currObj, bkmrksArr) {
+    if (searchResultNodesArr.length > 0) {
+        var searchObj = searchResultNodesArr.pop();
+        if (searchObj.title == currObj.tags[0] && searchObj.parentId == importFolderId) {
+            chrome.bookmarks.create({
+                'parentId': searchObj.id,
+                'title': currObj.title,
+                'url': currObj.url
+            });
+            outerFunc(importFolderId, bkmrksArr);
+        } else {
+            innerFunc(importFolderId, searchResultNodesArr, currObj, bkmrksArr);
         }
-    }
-  );
-}
 
-// Function used only for debugging purposes
-// Print to console the all the contents of the chrome bookmark nodes
-function traverseNodesRecursively(){
-  chrome.bookmarks.getTree(
-    //call back function per the specification of the .getTree method
-    function(node){
-      //declare and implement the helper function that traverses through nodes recursively
-      function recursiveImpl(node){
-        if(node.children){
-          if(node.title){
-            console.log('Folder is: ' + node.title + ' and size is: ' + node.children.length);
-          }
-          var i; //Take out this variable declaration and the script works but it becomes erratic and doesnt complete the traversal.
-          for(i=0; i < node.children.length; i++){
-            recursiveImpl(node.children[i]);
-          }
-        }
-        else{
-          if(node.title){
-              console.log('Bookmarks is: ' + node.title);
-          }
-        }
-      }
-      //end function
-
-      //call function and pass the base parent node of the chrome browser. This function can also be declared outside the call back function...
-      recursiveImpl(node[0]); //node[0] is the base for common chrome browser settings. It includes the children node 'Bookmarks Bar' and 'Other Bookmarks'
+    } else {
+        chrome.bookmarks.create({
+                'parentId': importFolderId,
+                'title': currObj.tags[0]
+            },
+            function(newFolder) {
+                chrome.bookmarks.create({
+                    'parentId': newFolder.id,
+                    'title': currObj.title,
+                    'url': currObj.url
+                });
+                outerFunc(importFolderId, bkmrksArr);
+            });
     }
-    //end call back function
-  );
 }
 
 // Add EventListener for the import button
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("import_button").addEventListener("click", importBookmarks);
+    document.getElementById("import_button").addEventListener("click", importBkmrks);
 });
